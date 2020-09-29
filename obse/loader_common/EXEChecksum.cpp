15f8bf0a12f9ca3bdb116a2757b301eebdfbbdf5
llde <lorenzofer@live.it>
2020-09-29 19:52:54 +0200

    Added Polish antology version checksum

--- obse/loader_common/EXEChecksum.cpp
+++ obse/loader_common/EXEChecksum.cpp
@@ -270,6 +270,7 @@ bool TestChecksum(const char * procName, std::string * dllSuffix, ProcHookInfo *
 				case 0x4F11C5C5:	// 1.2.0.416 hungarian unofficial v2
 				case 0xE243684D:    // 1.2.0.416 GOG version
 				case 0x299C4CB4:    // 1.2.0.416 GOTY Polish version by RPGowo 
+                case 0x9E284932:    // 1.2.0.416 Polish Antology by Cenega
 					*dllSuffix = "1_2_416";
 					result = true;
 					break;
