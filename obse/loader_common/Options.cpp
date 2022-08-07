25cf3c0ce97a79ed7623afd4afe1a6cf90878b27
llde <lorenzofersteam@live.it>
2022-08-07 18:31:15 +0200

    Readd -notimeout, fix debug build

--- obse/loader_common/Options.cpp
+++ obse/loader_common/Options.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <sstream> 
+
 #include <obse/loader_common/EXEChecksum.h>
 
 Options g_options;
@@ -49,18 +50,30 @@ bool Options::Read(LPSTR args)
 	auto argl = GetArgumentsList(args);
 	int	freeArgCount = 0;
 	bool alt_dll = false;
+	bool alt_exe = false;
+
 	for (std::string&  arg : argl){
-		if (alt_dll) {
+		if (alt_dll || alt_exe) {
 			if (arg.starts_with('-')) {
 				PrintError("Expected alternative DLL path got another option");
 				return false;
 			}
-			m_altDLL = arg;
-			alt_dll = false;
+			if (alt_dll) {
+				m_altDLL = arg;
+				alt_dll = false;
+			}
+			if (alt_exe) {
+				m_altEXE = arg;
+				alt_exe = false;
+			}
 		}
 		else if (arg == "-editor") { m_launchCS = true; }
 		else if (arg == "-h" || arg == "--help") { m_optionsOnly = true; }
 		else if (arg == "-altdll") { alt_dll = true; }
+		else if (arg == "-altexe") { alt_exe = true; }
+		else if (arg == "-old") { m_loadOldblivion = true; }
+		else if (arg == "-oldinject") { m_oldInject = true; }
+		else if (arg == "-notimeout") { m_threadTimeout = INFINITE; }
 		else {
 			PrintError("Unsupported option %s", arg.c_str());
 			return false;
@@ -83,23 +96,23 @@ void Options::PrintUsage(void)
 	_MESSAGE("  -h, -help - print this options list");
 	_MESSAGE("  -editor - launch the construction set");
 	_MESSAGE("  -old - load oldblivion");
-	_MESSAGE("  -priority <level> - set the launched program\'s priority");
-	_MESSAGE("    above_normal");
-	_MESSAGE("    below_normal");
-	_MESSAGE("    high");
-	_MESSAGE("    idle");
-	_MESSAGE("    normal");
-	_MESSAGE("    realtime");
+//	_MESSAGE("  -priority <level> - set the launched program\'s priority");
+//	_MESSAGE("    above_normal");
+//	_MESSAGE("    below_normal");
+//	_MESSAGE("    high");
+//	_MESSAGE("    idle");
+//	_MESSAGE("    normal");
+//	_MESSAGE("    realtime");
 	_MESSAGE("  -altexe <path> - set alternate exe path");
 	_MESSAGE("  -altdll <path> - set alternate dll path");
 	_MESSAGE("  -notimeout - wait forever for oblivion to launch");
 	_MESSAGE("               this overrides the default five second timeout");
 	_MESSAGE("  -crconly - just print the crc of the EXE, don't launch anything");
-	_MESSAGE("  -nosync - WARNING: USING THIS OPTION MAY CAUSE BUGS AND OTHER RANDOM BEHAVIOR");
-	_MESSAGE("            disable synchronization when injecting the dll");
-	_MESSAGE("            this is only intended as a compatibility hack for WINE");
-	_MESSAGE("  -waitforclose - wait for the launched program to close");
-	_MESSAGE("                  designed for use with AlacrityPC and similar");
+//	_MESSAGE("  -nosync - WARNING: USING THIS OPTION MAY CAUSE BUGS AND OTHER RANDOM BEHAVIOR");
+//	_MESSAGE("            disable synchronization when injecting the dll");
+//	_MESSAGE("            this is only intended as a compatibility hack for WINE");
+//	_MESSAGE("  -waitforclose - wait for the launched program to close");
+//	_MESSAGE("                  designed for use with AlacrityPC and similar");
 	_MESSAGE("  -oldinject - use the original injection method for the editor");
 }
 
